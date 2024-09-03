712cff10a74ccff10bf28cebcda70894a9cbe385
lincontroy <lincolnmunene37@gmail.com>
2024-09-03 08:17:08 +0300

    Fixed case sensitivity

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -5,8 +5,8 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width,initial-scale=1,shrink-to-fit=no">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
-  <title>Car Repair Service (Home 1) – ProMotors – Car Service & Detailing Template</title>
-  <link rel="shortcut icon" href="assets/images/site_logo/favourite_icon.svg">
+  <title>Sichey Automotive</title>
+  <link rel="shortcut icon" href="assets/images/site_logo/sichey.png">
   <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
   <link rel="stylesheet" type="text/css" href="assets/css/fontawesome.css">
   <link rel="stylesheet" type="text/css" href="assets/css/animate.css">
