b53456fcacf1b01540ae17af08deeff2959fb755
lincontroy <lincolnmunene37@gmail.com>
2024-09-03 08:20:08 +0300

    Fixed favicon

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -6,7 +6,7 @@
   <meta name="viewport" content="width=device-width,initial-scale=1,shrink-to-fit=no">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <title>Sichey Automotive</title>
-  <link rel="shortcut icon" href="assets/images/site_logo/sichey.png">
+  <link rel="shortcut icon" href="assets/images/favicon.ico">
   <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
   <link rel="stylesheet" type="text/css" href="assets/css/fontawesome.css">
   <link rel="stylesheet" type="text/css" href="assets/css/animate.css">
