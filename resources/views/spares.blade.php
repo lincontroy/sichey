4aaf778557d9ba39ce0023a4917fe9e53141d0b0
lincontroy <lincolnmunene37@gmail.com>
2024-09-12 12:24:16 +0300

    fixed the data issue

--- resources/views/spares.blade.php
+++ resources/views/spares.blade.php
@@ -13,21 +13,24 @@
 <section class="portfolio_section section_space_lg">
     <div class="container">
 
-    <p class="wow" data-splitting>We stock and distribute parts for all Case IH tractor models. We also are distributors of spares and parts for KIA, Renault, Hyundai motor vehicles.</p>
-    <br><hr>   
-    <div class="row">
-            <!-- First row -->
+        <p class="wow" data-splitting>We stock and distribute parts for all Case IH tractor models. We also are
+            distributors of spares and parts for KIA, Renault, Hyundai motor vehicles.</p>
+        <br>
+        <hr>
+        <div class="row">
+
             <div class="col-lg-4 col-md-6 col-sm-6">
                 <div class="team_expert_item">
                     <div class="team_expert_image">
-                        <img src="assets/images/b1.jpg" alt="ProMotors - Car Repair Service">
+                        <img src="assets/images/kamatsu.png" alt="ProMotors - Car Repair Service">
                     </div>
                 </div>
             </div>
+
             <div class="col-lg-4 col-md-6 col-sm-6">
                 <div class="team_expert_item">
                     <div class="team_expert_image">
-                        <img src="assets/images/b2.jpg" alt="ProMotors - Car Repair Service">
+                        <img src="assets/images/b4.jpg" alt="ProMotors - Car Repair Service">
                     </div>
                 </div>
             </div>
@@ -41,38 +44,52 @@
                     </div>
                 </div>
             </div>
-            
+
+           
+
+
+            <!-- First row -->
+
+
+
+
         </div>
         <div class="row">
-
         <div class="col-lg-4 col-md-6 col-sm-6">
                 <div class="team_expert_item">
                     <div class="team_expert_image">
-                        <img src="assets/images/b4.jpg" alt="ProMotors - Car Repair Service">
+                        <img src="assets/images/b2.jpg" alt="ProMotors - Car Repair Service">
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 col-sm-6">
+                <div class="team_expert_item">
+                    <div class="team_expert_image">
+                        <img src="assets/images/b1.jpg" alt="ProMotors - Car Repair Service">
                     </div>
                 </div>
             </div>
 
-       
+
             <!-- Second row -->
-            <div class="col-lg-4 col-md-6 col-sm-6">
+            <!-- <div class="col-lg-4 col-md-6 col-sm-6">
                 <div class="team_expert_item">
                     <div class="team_expert_image">
                         <img src="assets/images/b5.jpg" alt="ProMotors - Car Repair Service">
                     </div>
                 </div>
-            </div>
+            </div> -->
             <div class="col-lg-4 col-md-6 col-sm-6">
                 <div class="team_expert_item">
                     <div class="team_expert_image">
-                        <img src="assets/images/b6.jpg" alt="ProMotors - Car Repair Service">
+                        <img src="assets/images/cont.jpg" alt="ProMotors - Car Repair Service">
                     </div>
                 </div>
             </div>
-            </div>
-           
-            
-        
+        </div>
+
+
+
     </div>
 </section>
 <section class="blog_standard_section section_space_lg">
@@ -83,17 +100,19 @@
                             alt="ProMotors - Blog Image"></a>
                     <div class="item_content">
                         <br>
-                        
+
                         <h3 class="item_title"><a href="">SOURCING AND DISTRIBUTION</a>
                         </h3>
-                        <p>We source for and distribute a wide range of parts and spares from overseas suppliers. Incase you require a part that is not available locally, be sure we will find it on your behalf in the international market.</p>
+                        <p>We source for and distribute a wide range of parts and spares from overseas suppliers. Incase
+                            you require a part that is not available locally, be sure we will find it on your behalf in
+                            the international market.</p>
 
                     </div>
                 </div>
 
-                
+
             </div>
-            
+
         </div>
     </div>
 </section>
